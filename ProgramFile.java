dce87a7007b298d2da0675d4563206219490329c
Sharayu <sharayuraut18@gmail.com>
2020-02-26 02:47:40 +0530

    minor login changes in bran01
    Welcome admin changes in feature branch 01

--- ProgramFile.java
+++ ProgramFile.java
@@ -21,7 +21,7 @@ public class LoginTestFF
 		WebElement userLogin=dr.findElement(By.id("btnLogin"));
 		userLogin.click();
 		
-		dr.findElement(By.xpath("//a[@id ='welcome Admin']"))                                                                                              ;
+		dr.findElement(By.xpath("//a[@id ='Welcome Admin']"))                                                                                              ;
 	}
 
 }
